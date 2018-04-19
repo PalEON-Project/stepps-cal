8d1cd12c5a5f5d713269f3e1903722a1e91a11b2
SimonGoring <simon.j.goring@gmail.com>
2018-04-19 16:05:09 -0700

    Removed the northwestern vignette, and added documentation for the `downloads` object of midwestern pollen data.

---
+++ R/downloads.R
@@ -0,0 +1,8 @@
+#' @title Neotoma download data for the Upper Midwestern United States
+#' @docType data
+#' @author Simon Goring
+#' @description A set of \code{download} objects from the Neotoma Paleoecological Database
+#' to be used in the STEPPS package vignette, or as part of code examples.
+#' @source \url{http://neotomadb.org}
+
+"downloads"
